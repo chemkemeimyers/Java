313313a66f9342e092917ec41db96ad5b35891b1
chemkemeimyers <kemei8@gmail.com>
2025-01-24 06:16:26 -0800

    More on classes

--- Classes/Insect.java
+++ Classes/Insect.java
@@ -1,25 +1,46 @@
+
+
 public class Insect{
     private double weight;
     private int x;
     private int y;
 
+    public static final int DEFAULT_X = 0;
+    public static final int DEFAULT_Y = 0;
     private final double DIST_WEIGHT_LOSS_FACTOR = 0.001;
-
+    private static int population = 0;
+    private static final String[] FACTS = {
+        "The two main groups of insects are winged and wingless",
+        "There are more than 1 million insect species",
+        "Insects are cold-blooded",
+        "Spiders are not considered insects"
+    };
+    //Constructors
+    public Insect(double initWeight)
+    {
+        this(initWeight, DEFAULT_X, DEFAULT_Y);
+    }
     public Insect(double initWeight, int initX, int initY)
     {
         weight = initWeight;
         x = initX;
         y = initY;
+        population+=1;
     }
+
     //test method
     public static void main(String[] main)
     {
         //Insect buzz1 = new Insect();
        // Insect buzz2 = new Insect();
+        System.out.println("Insect Population: " + population);
         Insect bug1 = new Insect(10, 100,  90);
+        System.out.println("Insect Population: " + population);
         Insect bug2 = new Insect(4, -300,  400);
+        System.out.println("Insect Population: " + population);
         bug1.move(1, 10);
         bug2.move(-300, 400);
+        System.out.println(produceRandomFacts());
     }
 
     //methods
@@ -47,5 +68,45 @@ public class Insect{
     private static double calculateDistance(double x1, double y1, double x2, double y2)
     {
         return Math.sqrt((y2-y1)*(y2-y1) + (x2-x1)*(x2-x1));
+    }
+
+    public static String produceRandomFacts()
+    {
+        int index = (int)(Math.random() * ((FACTS.length - 1) + 1));
+        return FACTS[index];
+    }
+    public double getWeight()
+    {
+        return weight;
+    }
+    public int getX()
+    {
+        return x;
+    }
+    public void setX(int newX)
+    {
+        if(isLegalNewX(newX))
+            x = newX;
+    }
+    public int getY()
+    {
+        return y;
+    }
+    public void setY(int newY)
+    {
+        if(isLegalNewX(newY))
+            y = newY;
+    }
+    public static int getPopulation()
+    {
+        return population;
+    }
+    public static boolean isLegalNewX(int newX)
+    {
+        return newX >= 0;
+    }
+    public static boolean isLegalNewY(int newY)
+    {
+        return newY >= 0;
     }
 }
---
+++ Classes/InsectClient.java
@@ -0,0 +1,21 @@
+public class InsectClient{
+    public static void main(String[] args) {
+        System.out.println(Insect.produceRandomFacts());
+
+        Insect bug1 = new Insect(13, 31, 0);
+        System.out.println(bug1.getWeight());
+        System.out.println(bug1.getX());
+        bug1.setX(-314);
+        System.out.println(bug1.getX());
+        bug1.setX(133);
+        System.out.println(bug1.getX());
+        System.out.println(bug1.getY());
+        System.out.println("Insect population: " + Insect.getPopulation());
+
+        Insect bug2 = new Insect(31);
+        System.out.println(bug2.getWeight());
+        System.out.println(bug2.getX());
+        System.out.println(bug2.getY());
+        System.out.println("Insect population: " + Insect.getPopulation());
+    }
+}
