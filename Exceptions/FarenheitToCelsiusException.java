b138aaa5597acd56730bae0b19634dfad76288e1
chemkemeimyers <kemei8@gmail.com>
2025-02-18 07:02:10 -0800

    Create FarenheitToCelsiusException.java

---
+++ Exceptions/FarenheitToCelsiusException.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class FarenheitToCelsiusException{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Fahrenheit value:");
+        int fahrenheit = input.nextInt();
+        double celsius = (5.0/9)*(fahrenheit-32);
+        System.out.printf("Fahrenheit: %d\n", fahrenheit);
+        System.out.printf("Celcius: %.1f\n",celsius);
+    }
+}
