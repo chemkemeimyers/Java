329cc78aa57d94e2646b5a3e4a8cb82f7594c52d
chemkemeimyers <kemei8@gmail.com>
2025-02-24 07:23:23 -0800

    Create FahrenheitToCelsiusExceptions_R4.java

---
+++ Exceptions/FahrenheitToCelsiusExceptions_R4.java
@@ -0,0 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class FahrenheitToCelsiusExceptions_R4
+{
+    public static void main(String[] args) {
+        {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter a Fahrenheit value: ");
+            try{
+                int fahrenheit = input.nextInt();
+                double celsius = (5.0/9) * (fahrenheit - 32);
+                System.out.printf("Fahrenheit: %d\n", fahrenheit);
+                System.out.printf("Celcius: %,1f\n", celsius);
+
+                if(fahrenheit == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                double x = 1331/fahrenheit;
+            }
+            catch(InputMismatchException ime)
+            {
+                System.out.println("Sorry, that wasn't an int");
+                System.out.println("Please re-run the program again");
+            }
+            catch(DivideByZeroException dze)
+            {
+                System.out.println("Oops. You entered an invalid number");
+                System.out.println(dze.getMessage());
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
