50613d2ae8c023491637549afa2b58c719037457
chemkemeimyers <kemei8@gmail.com>
2025-02-26 06:12:22 -0800

    Upload interface and arrays example

---
+++ Exceptions/Canine.java
@@ -0,0 +1,16 @@
+public abstract class Canine implements Groomable
+{
+    protected double size;
+
+    public Canine(double size)
+    {
+        this.size = size;
+    }
+
+    public void bark()
+    {
+        System.out.println("Woof Woof");
+    }
+
+    public abstract void groom();
+}
---
+++ Exceptions/Wolf.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+public class Wolf extends Canine implements Comparable<Wolf>
+{
+    protected int rank;
+    public Wolf(double size, int rank)
+    {
+        super(size);
+        this.rank = rank;
+    }
+
+    public int getRank()
+    {
+        return rank;
+    }
+
+    public void bark(int barkMultiple)
+    {
+        for(int i=1; i<=barkMultiple; i++)
+        {
+            super.bark();
+        }
+    }
+
+    public void groom()
+    {
+        System.out.println("lick");
+    }
+
+    public int compareTo(Wolf anotherWolf)
+    {
+        return -(rank - anotherWolf.rank);
+    }
+
+    public String toString()
+    {
+        return ("Rank " + rank + ", Size " + size);
+    }
+
+    public void pay()
+    {
+        System.out.println("Grrrr! Warning, don't try and chase me for payment. I'm a wolf. Run Run Run!");
+    }
+
+    public static void main(String[] args)
+    {
+        Wolf[] pack = {
+            new Wolf(17.1, 2),
+            new Wolf(3, 10),
+            new Wolf(9.2, 7),
+            new Wolf(9.1, 8),
+            new Wolf(17.01, 3),
+            new Wolf(16.2, 1),
+            new Wolf(16, 4),
+            new Wolf(16, 5),
+            new Wolf(10, 6),
+            new Wolf(5, 9)
+        };
+
+        System.out.println("Unsorted Pack: " + Arrays.toString(pack));
+        Arrays.sort(pack);
+        System.out.println("Sorted Pack: " + Arrays.toString(pack));
+            }
+
+}
