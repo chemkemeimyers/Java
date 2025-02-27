302552f16455e0bc7374184873d605ee0735a926
chemkemeimyers <kemei8@gmail.com>
2025-02-27 06:14:26 -0800

    Upload Dog.java - Practise extending abstract classes, defining exceptions

---
+++ Exceptions/Dog.java
@@ -0,0 +1,72 @@
+public class Dog extends Pet{
+    protected double droolRate;
+
+    public Dog(String name, double health, int painLevel, double droolRate)
+    {
+        super(name, health, painLevel);
+        if(droolRate <= 0)
+            this.droolRate = 0.5;
+        else 
+            this.droolRate = droolRate;
+    }
+
+    public Dog(String name, double health, int painLevel)
+    {
+        super(name, health, painLevel);
+        this.droolRate = 5.0;
+    }
+
+    public double getDroolRate()
+    {
+        return this.droolRate;
+    }
+
+    @Override
+    public int treat()
+    {
+        super.heal();
+        try
+        {
+            if(this.droolRate < 3.5)
+            {
+                return (int) (painLevel*2/health);
+            }
+            else if(3.5 <= this.droolRate && this.droolRate <= 7.5)
+            {
+                return (int) (painLevel/health);
+            } 
+            else 
+            {
+                return (int) (painLevel/(health * 2));
+            }
+        }
+        catch(ArithmeticException e)
+        {
+            throw new ArithmeticException("Health is zero, causing a divide by zero exception");
+        }
+    }
+
+    @Override
+    public void speak()
+    {
+        super.speak();
+        String message = "";
+        for(int i=0; i< painLevel; i++)
+        {
+            message=message+"bark";
+            if(i<painLevel-1)
+                message=message+" ";
+        }
+        if(this.painLevel > 5)
+            System.out.println(message.toUpperCase());
+        else
+            System.out.println(message);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        Dog toCompare = (Dog)o;
+        return super.equals(o) && toCompare.getDroolRate() == this.droolRate; 
+    }
+}
