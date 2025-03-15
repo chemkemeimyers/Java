099936f25f8074857fe1e09401e9788342e563ac
chemkemeimyers <kemei8@gmail.com>
2025-03-15 08:31:56 -0700

    Create PlayList.java

---
+++ Lists/PlayList.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+public class PlayList{
+    public static ArrayList<String> playlist = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        {
+            playlist.add("we");
+
+            playlist.add("didn't");
+
+            playlist.add("start");
+
+            playlist.add("the");
+
+            playlist.add("fire");
+
+            playlist.add("didn't");
+
+            System.out.println(playlist.contains("didn't"));
+
+            int sum = new Integer(3);
+        }
+    }
+}
