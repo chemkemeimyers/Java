3879cce3478dce69c2a4a4a1f626f210d57ba67b
Moureen Kemei Myers <kemei8@gmail.com>
2025-04-02 21:44:20 -0700

    Create HelloWorldAnon.java

---
+++ JavaFX/HelloWorldAnon.java
@@ -0,0 +1,36 @@
+import javafx.application.application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorldAnon extends Application{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    public void start(Stage mainStage){
+        mainStage.setTitle("Hello World Program(Anon)");
+
+        Button btn = new Button();
+        btn.setText("Print Hello World!");
+
+        EventHandler<ActionEvent> handler = new EventHandler<>(){
+            public void handle(ActionEvent event){
+                System.out.println("Hello World");
+            }
+        }
+        btn.setOnAction(handler);
+
+        StackPane root = new StackPane();
+        root.getCHildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 300);
+        mainStage.setScene(scene);
+        mainStage.show();
+
+    }
+}
