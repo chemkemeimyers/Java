2487dee3c1963090018298de64019fd629d0ea74
chemkemeimyers <kemei8@gmail.com>
2025-05-24 14:50:56 -0700

    Add rectangle class

---
+++ ArrayLists_LinkedLists_Stacks_Queues/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle {
+    private double length, width;
+    public Rectangle(double l, double width) {
+        length = l;
+        width = width;
+    }
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(50, 100);
+    }
+}
