37fb51add5f96e3128b002c822e70520504155cc
chemkemeimyers <kemei8@gmail.com>
2025-06-28 10:21:24 -0700

    Array list implementation

---
+++ ArrayLists_LinkedLists_Stacks_Queues/ArrayList.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+
+Public class MyArrayList<T>{
+    private Object[] backingArray;
+    private int size = 0;
+    private static final int INITIAL_CAPACITY = 10;
+
+    public MyArrayList(){
+        backingArray = new Object[INITIAL_CAPACITY];
+    }
+
+
+    //Check for space for a new element
+    private void CapacityPresentForNewElement()
+    {
+        if (size == backingArray.length)
+        {
+            backingArray = Arrays.copyOf(backingArray, backingArray.length * 2);
+        }
+    }
+
+    //Add element at end
+    public void addToEnd(T element)
+    {
+        CapacityPresentForNewElement();
+        backingArray[size] = element;
+        size++;
+    }
+
+    private void checkBounds(int index)
+    {
+        if(index < 0 || index >= size)
+        {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds."); 
+        }
+    }
+
+    //Get element at index
+    @SuppressWarnings("unchecked")
+    public T get(int index){
+        checkBounds(index);
+        return (T) backingArray[index];
+    }
+
+    //Remove element at index
+    @SuppressWarnings("unchecked")
+    public T remove(int index)
+    {
+        checkBounds(index);
+
+        T removed = (T) backingArray[index];
+
+        //Shift elements left
+        for(int i = index; i< size-1; i++)
+        {
+            backingArray[i] = backingArray[i+1];
+        }
+
+        backingArray[size-1]=null;
+        size--;
+
+        return removed
+    }
+
+    public int size()
+    {
+        return size;
+    }
+
+    //for printing
+    @Override
+    public String toString()
+    {
+        return Arrays.toString(Arrays.copyOf(backingArray, size))
+    }
+}
