a337c6c7d18319d129c5a6ad46dbfac39f1e6bdf
chemkemeimyers <kemei8@gmail.com>
2025-07-04 09:39:44 -0700

    Add comments to factorial program

--- ArrayLists_LinkedLists_Stacks_Queues/Factorial.java
+++ ArrayLists_LinkedLists_Stacks_Queues/Factorial.java
@@ -1,17 +1,18 @@
 public class Factorial{
+    //Class to implement factorial method which uses recursion
     
     public static int factorial(int n) throws IllegalArgumentException
     {
-        if(n < 0) //base case
+        if(n < 0) //Exception case
         {
             throw new IllegalArgumentException();
         }
         else if(n == 0)
         {
-            return 1;
+            return 1; //base case 
         }
         else{
-            return n*factorial(n-1);
+            return n*factorial(n-1); //recursive case
         }
     }
 
