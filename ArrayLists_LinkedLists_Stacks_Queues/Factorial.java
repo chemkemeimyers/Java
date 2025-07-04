39b26901ce8e67d1b431a66ce17080ef5838e760
chemkemeimyers <kemei8@gmail.com>
2025-07-04 09:35:32 -0700

    Add recursive factorial program

---
+++ ArrayLists_LinkedLists_Stacks_Queues/Factorial.java
@@ -0,0 +1,24 @@
+public class Factorial{
+    
+    public static int factorial(int n) throws IllegalArgumentException
+    {
+        if(n < 0) //base case
+        {
+            throw new IllegalArgumentException();
+        }
+        else if(n == 0)
+        {
+            return 1;
+        }
+        else{
+            return n*factorial(n-1);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int num = 4;
+        int result = factorial(num);
+        System.out.println("Factorial of " + num + " is " + result);
+    }
+}
